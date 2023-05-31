f54e3b45423f9da7a3bf65f8740b2dd9df0c639a
Jaime Escalona <jaescalo@akamai.com>
2023-05-31 10:25:21 -0600

    define akamai creds vars in tf

--- github-actions-terraform/variables.tf
+++ github-actions-terraform/variables.tf
@@ -1,7 +1,7 @@
-variable "edgerc_path" {
-  type    = string
-  default = "~/.edgerc"
-}
+variable "akamai_client_secret" { }
+variable "akamai_host" { }
+variable "akamai_access_token" { }
+variable "akamai_client_token" { }
 
 variable "config_section" {
   type    = string
