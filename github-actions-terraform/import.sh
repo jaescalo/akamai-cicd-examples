b7caba8fcc84b58186fabf2a7258a56ffed2260e
Jaime Escalona <jaescalo@akamai.com>
2023-05-31 11:05:56 -0600

    TF import

---
+++ github-actions-terraform/import.sh
@@ -0,0 +1,2 @@
+terraform -chdir=./github-actions-terraform import akamai_edge_hostname.jaescalo-test-edgekey-net ehn_4874468,ctr_1-1NC95D,grp_71960
+terraform -chdir=./github-actions-terraform import akamai_property.gitops-prod-demo-com prp_833332,ctr_1-1NC95D,grp_71960,LATEST
